01b20a511230ee088e26f15d16ca50812be0e296
priyan-shu270 <priyanshugupta1567@gmail.com>
2026-09-22 22:26:05 +0530

    added practice

---
+++ practice/printnto1.java
@@ -0,0 +1,13 @@
+package practice;
+
+import java.util.Scanner;
+
+public class printnto1 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner (System.in);
+        int n = scn.nextInt();
+        for(int i =n; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
